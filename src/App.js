4afacc645d6fc43569a07fc16503e8a4c39dfbda
Fareed <fareedrajpoot324@gmail.com>
2024-01-24 00:32:05 +0500

    react form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import HorizontalLinearStepper from "./components/HorizontalLinearStepper";
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} style={{padding:"30px 30px 30px 30px"}} >
+        <HorizontalLinearStepper />
+        </Box>
+       </Container> 
   );
 }
 
